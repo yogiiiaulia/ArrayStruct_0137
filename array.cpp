5e4daaf7ac2eabe3aceadc0de529e8189c7cd2d0
yogiiiaulia <yogiauliaardiano@gmail.com>
2025-04-20 15:05:02 +0700

    pertengahan

--- array.cpp
+++ array.cpp
@@ -1,3 +1,11 @@
 #include <iostream>
 using namespace std;
 
+// deklarasi array tanpa ukuran, wajib diisi datanya
+int nilai[] = {1, 2, 4, 7};
+
+// deklarasi array dengan ukuran dan diberi nilai awal
+int nilaiDua[3] = {1, 2, 3};
+
+// deklarasi array dengan ukuran, tanpa nilai awal
+string nama[4];

13c35d5e19a542a4733b7a1ea5b9482e8f2777c0
yogiiiaulia <yogiauliaardiano@gmail.com>
2025-04-22 11:45:59 +0700

    selesai

--- struct.cpp
+++ struct.cpp
@@ -7,3 +7,24 @@ struct Mahasiswa
     string nim;
     string alamat;
 };
+
+int main()
+{
+    // Membuat object struct Mahasiswa
+    Mahasiswa mhs;
+
+    cout << "Masukan nim = ";
+    cin >> mhs.nim;
+    cout << "Masukan nama = ";
+    cin >> mhs.nama;
+    cout << "Masukan alamat = ";
+    cin >> mhs.alamat;
+
+    cout << endl;
+    cout << "Menampilkan Data" << endl;
+    cout << endl;
+
+    cout << "NIM = " << mhs.nim << endl;
+    cout << "Nama = " << mhs.nama << endl;
+    cout << "Alamat = " << mhs.alamat << endl;
+}

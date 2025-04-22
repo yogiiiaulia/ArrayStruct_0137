a8069f4a4e82209900eea7da015c66f6439a8508
yogiiiaulia <yogiauliaardiano@gmail.com>
2025-04-22 11:06:02 +0700

    setengah

--- nested_struct.cpp
+++ nested_struct.cpp
@@ -1,2 +1,15 @@
 #include <iostream>
 using namespace std;
+
+struct DetailAlamat
+{
+    string kota;
+    string provinsi;
+};
+
+struct Mahasiswa
+{
+    string nama;
+    string nim;
+    DetailAlamat alamat;
+};

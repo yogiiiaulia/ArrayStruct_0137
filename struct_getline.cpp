324e8990e5eb0cc6149d82a5e0814a8099bf368b
yogiiiaulia <yogiauliaardiano@gmail.com>
2025-04-22 11:43:22 +0700

    selesai

--- struct_getline.cpp
+++ struct_getline.cpp
@@ -14,3 +14,35 @@ struct Mahasiswa
     string nim;
     DetailAlamat alamat;
 };
+
+int main()
+{
+    // Membuat object struct dalam array
+    Mahasiswa mhs[2];
+    for (int i = 0; i < 2; i++)
+    {
+        cout << "Mahasiswa ke-" << i + 1 << endl;
+        cout << "Masukan nim = ";
+        cin >> mhs[i].nim;
+        cin.ignore();
+        cout << "Masukan nama = ";
+        getline(cin, mhs[i].nama);
+        cout << "Masukan kota = ";
+        cin >> mhs[i].alamat.kota;
+        cout << "Masukan provinsi = ";
+        cin >> mhs[i].alamat.provinsi;
+        cin.ignore();
+    }
+
+    cout << endl;
+    cout << "Menampilkan Data" << endl;
+    cout << endl;
+    for (int i = 0; i < 2; i++)
+    {
+        cout << "Mahasiswa ke-" << i + 1 << endl;
+        cout << "NIM = " << mhs[i].nim << endl;
+        cout << "Nama = " << mhs[i].nama << endl;
+        cout << "Kota = " << mhs[i].alamat.kota << endl;
+        cout << "Provinsi = " << mhs[i].alamat.provinsi << endl;
+    }
+}

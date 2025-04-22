01442f6da9ebf30e4ffd90d941a8823707ef9339
yogiiiaulia <yogiauliaardiano@gmail.com>
2025-04-22 11:42:42 +0700

    setengah

--- struct_getline.cpp
+++ struct_getline.cpp
@@ -1,3 +1,16 @@
 #include <iostream>
 #include <string>
 using namespace std;
+
+struct DetailAlamat
+{
+    string kota;
+    string provinsi;
+};
+
+struct Mahasiswa
+{
+    string nama;
+    string nim;
+    DetailAlamat alamat;
+};
